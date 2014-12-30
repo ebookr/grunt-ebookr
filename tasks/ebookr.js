b4015c96eb081c2696a357079fbcc4629ade1b17
Arne Hassel <arne.hassel@gmail.com>
2014-12-30 01:01:09 +0100

    Now also accepts global options

--- tasks/ebookr.js
+++ tasks/ebookr.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
     });
 
     // Iterate over all specified file groups.
-    var taskOptions = this.data.options || {};
+    var taskOptions = this.options() || {};
     var promises = this.files.map(function(f) {
       var src = f.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
